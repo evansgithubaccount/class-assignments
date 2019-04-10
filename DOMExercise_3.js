fe0a20054a4a9f4122255e316c06e635dac0918c
evansgithubaccount <48694606+evansgithubaccount@users.noreply.github.com>
2019-04-10 14:21:47 -0400

    DOM Exercise 3 HW

---
+++ DOMExercise_3.js
@@ -0,0 +1,14 @@
+let list = document.querySelector('ul.list');
+
+function addItem(){
+	let newInput = document.querySelector("input.newToDo").value;
+	newItem = document.createElement('li');
+	newItem.innerHTML = newInput;
+	list.appendChild(newItem);
+	document.querySelector('input.newToDo').value = "";
+}
+
+document.querySelector('ul').addEventListener('click',function(arg){
+		(arg.target).style.textDecoration = 'line-through';
+		setTimeout(function(){(arg.target).parentNode.removeChild(arg.target)},1000)
+	})
